3e759a41669cf59f3f1fd5521d23235869aee297
Jeremy-mfs <80481825+Jeremy-mfs@users.noreply.github.com>
2023-09-13 21:37:12 -0600

    RegistroColaborador
    Conexión BD completa, faltan validaciones del ingreso de los datos y ordenar los divs.

--- Web/public/guardar.js
+++ Web/public/guardar.js
@@ -4,7 +4,29 @@ function guardar(){
         apellidos: document.getElementById("apellidos").value,
         areaDestudio: document.getElementById("areaDestudio").value,
         carne: document.getElementById("carne").value,
-        contrasena: document.getElementById("contrasena").value
+        contrasena: document.getElementById("contrasena").value,
+        correoEstudiante: document.getElementById("correoEstudiante").value
+
+       
+        
+    })
+    .then((docRef) => {
+        alert("Registro correcto")
+    })
+    .catch((error) => {
+        alert("Registro incorrecto")
+    });
+
+}
+
+function guardarColaborador(){
+    db.collection("colaboradores").add({
+        nombreColaborador: document.getElementById("nombreColaborador").value,
+        apellidosColaborador: document.getElementById("apellidosColaborador").value,
+        numeroTelefonoC: document.getElementById("numeroTelefonoC").value,
+        correoColaborador: document.getElementById("correoColaborador").value,
+        descripcionColaborador: document.getElementById("descripcionColaborador").value,
+        contraseñaColaborador: document.getElementById("contraseñaColaborador").value
 
        
         
@@ -17,4 +39,6 @@ function guardar(){
     });
 
 
+
 }
+
